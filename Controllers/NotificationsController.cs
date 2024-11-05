34f2341b7e78a2fa346f22aa442edd56d6c3d0fc
murali <dmurali2002@gmail.com>
2024-11-05 20:55:30 +0530

    initial commit

---
+++ Controllers/NotificationsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using NotificationsAPI.Hubs;
+
+namespace NotificationsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public NotificationsController(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostNotification([FromBody] string message)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveNotification", message);
+            return Ok(new { Message = "Notification sent" });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using NotificationsAPI.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowVueApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:8080") 
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+app.UseCors("AllowVueApp");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<NotificationHub>("/notifications").RequireCors("AllowVueApp"); ;
+
+app.Run();
